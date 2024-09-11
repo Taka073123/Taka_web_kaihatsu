6d23ba0a0401faef1786af1db2b30bac544580e3
Taka073123 <kd1399118@st.kobedenshi.ac.jp>
2024-09-11 11:58:06 +0900

    Add files via upload

---
+++ sample12-2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>BMI</title>
+</head>
+<body>
+    </form>
+    <?php
+    session_start();
+    $name = $_SESSION['name'];
+    $email = $_SESSION['email'];
+    echo "名前: " . $name . "<br>";
+    echo "Email" . $email . "<br>";
+    echo "セッションID: " . session_id() . "<br>";
+    echo "セッション名: " . session_name() . "<br>";
+
+    ?>
+</body>
+</html>
